7ce5de4efc325589b79fe8e89c337d05fcee0676
史 翔新 <shi_xiangxin@mages.co.jp>
2016-06-02 13:38:28 +0900

    Add custom controllers and views

---
+++ ContainerViewControllerSample/Source/Views/View.swift
@@ -0,0 +1,21 @@
+//
+//  View.swift
+//  ContainerViewControllerSample
+//
+//  Created by 史　翔新 on 2016/06/02.
+//  Copyright © 2016年 Crazism. All rights reserved.
+//
+
+import UIKit
+
+class View: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
