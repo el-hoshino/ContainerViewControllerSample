eef99a104ca26bea898956b826287223809e5a47
史 翔新 <shi_xiangxin@mages.co.jp>
2016-06-02 13:34:16 +0900

    init commit

---
+++ ContainerViewControllerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContainerViewControllerSample
+//
+//  Created by 史　翔新 on 2016/06/02.
+//  Copyright © 2016年 Crazism. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# ContainerViewControllerSample
+This is a sample project to present how to implement a child view controller into a container view controller, and implement the child view controller's custom view into the container view controller's custom view
